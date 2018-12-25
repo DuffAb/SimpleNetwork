82a5ce8ef03f95264da89aef9987d2e5f9148f8d
梁德锋 <defeng.liang@3glasses.com>
2018-12-25 20:15:30 +0800

    this version compile ok

--- src/CrossPlatform.h
+++ src/CrossPlatform.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _CROSS_PLATFORM_H_
 #define _CROSS_PLATFORM_H_
 
@@ -51,57 +50,3 @@ static const int SOCKET_ERROR = -1;
 #endif
 
 #endif  /*_CROSS_PLATFORM_H_*/
-=======
-#ifndef _CROSS_PLATFORM_H_
-#define _CROSS_PLATFORM_H_
-
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <vector>
-
-#ifdef _WIN32
-#include <WinSock2.h>
-#include <ws2tcpip.h>
-#include <basetsd.h>
-#include <signal.h>
-#include <io.h>
-#elif __linux__
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#endif
-
-
-// Cross-platform Function
-#ifdef _WIN32
-#define UW_ErrMessage(x) (x)
-#define UW_Read(x, y, z)   _read(x, y, z)
-#define UW_Write(x, y, z)  _write(x, y, z)
-#define TCP_Close(x)       closesocket(x)
-#define TCP_Error(x)       printf(x)
-#elif __linux__
-#define UW_ErrMessage(x)   hstrerror(x)
-#define UW_Read(x, y, z)   read(x, y, z)
-#define UW_Write(x, y, z)  write(x, y, z)
-#define TCP_Close(x)       close(x)
-#define TCP_Error(x)       herror(x)
-#endif
-
-
-// Cross-platform Type
-#ifdef _WIN32
-typedef SSIZE_T ssize_t;
-typedef SOCKET  SocketHandle;
-#elif __linux__
-typedef int     SocketHandle;
-static const SocketHandle INVALID_SOCKET = -1;
-static const int SOCKET_ERROR = -1;
-#endif
-
-#endif  /*_CROSS_PLATFORM_H_*/
->>>>>>> b42339f86429495f7f8c0eabe515682d6c9388ad
--- src/OWin32Socket.cpp
+++ src/OWin32Socket.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /************************************************************************************
 
 PublicHeader:   n/a
@@ -83,92 +82,6 @@ OTCPSocket::~OTCPSocket()
 }
 
 // void (*signal(int signo, void (*func)(int)))(int);
-=======
-/************************************************************************************
-
-PublicHeader:   n/a
-Filename    :   OWin32Socket.cpp
-Content     :   Windows-specific socket-based networking implementation
-Created     :   December 12, 2018
-Authors     :   defeng.liang
-************************************************************************************/
-
-#include "OSocket.h"
-
-// WSAStartupSingleton
-class OWSAStartupSingleton
-{
-public:
-	static void AddRef(void);
-	static void DeRef(void);
-protected:
-	static int RefCount;
-};
-
-// 类外初始化静态类成员变量
-int OWSAStartupSingleton::RefCount = 0;
-
-// 实现静态类成员函数
-void OWSAStartupSingleton::AddRef()
-{
-	if (++RefCount == 1)
-	{
-		WSADATA wsaData;
-		const int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
-		if (err != 0)
-		{
-			std::cout << "[Socket] Unable to initialize Winsock " << err << std::endl;
-		}
-	}
-}
-
-void OWSAStartupSingleton::DeRef()
-{
-	if (RefCount > 0)
-	{
-		if (--RefCount == 0)
-		{
-			WSACleanup();
-			RefCount = 0;
-		}
-	}
-}
-
-void OBerkleySocket::OClose()
-{
-	if (_TheSocket != INVALID_SOCKET)
-	{
-		closesocket(_TheSocket);
-		_TheSocket = INVALID_SOCKET;
-	}
-}
-
-OTCPSocket::OTCPSocket() : _IsListenSocket(false)
-{
-	_AF_XXX = FamilyType_None;
-	OWSAStartupSingleton::AddRef();
-}
-
-OTCPSocket::OTCPSocket(FamilyType ft) : _IsListenSocket(false)
-{
-	_AF_XXX = ft;
-	OWSAStartupSingleton::AddRef();
-}
-
-OTCPSocket::OTCPSocket(FamilyType ft, SocketHandle handle) : _IsListenSocket(false)
-{
-	_AF_XXX = ft;
-	_TheSocket = handle;
-	OWSAStartupSingleton::AddRef();
-}
-
-OTCPSocket::~OTCPSocket()
-{
-	OWSAStartupSingleton::DeRef();
-}
-
-// void (*signal(int signo, void (*func)(int)))(int);
->>>>>>> b42339f86429495f7f8c0eabe515682d6c9388ad
 // 1.signal(int signo, void(*func)(int))
 //   signal是一个函数，有2个参数，第一个是int类型，第二个参数是一个函数指针
 // 2.void(*signal(int signo, void(*func) (int))) (int)
@@ -177,7 +90,6 @@ OTCPSocket::~OTCPSocket()
 //   typedef void(*sighandler_t)(int)
 //   sighandler_t 代表的就是 返回值是void，有一个int参数的函数指针
 // 4.最后就成了这样
-<<<<<<< HEAD
 //  sighandler_t signal(int signo, sighandler_t handler);
 sighandler_t OTCPSocket::OSetSIGHandler(OSIGParams* osig)
 {
@@ -209,37 +121,4 @@ sighandler_t OTCPSocket::OSetSIGHandler(OSIGParams* osig)
 	}
 	previousHandler = signal(signo, osig->handler);
 	return previousHandler;
-=======
-//  sighandler_t signal(int signo, sighandler_t handler);
-sighandler_t OTCPSocket::OSetSIGHandler(OSIGParams* osig)
-{
-	sighandler_t previousHandler = NULL;
-	int signo = 0;
-	switch (osig->signo)
-	{
-	case O_SIGINT:
-		signo = SIGINT;
-		break;
-	case O_SIGILL:
-		signo = SIGILL;
-		break;
-	case O_SIGFPE:
-		signo = SIGFPE;
-		break;
-	case O_SIGSEGV:
-		signo = SIGSEGV;
-		break;
-	case O_SIGTERM:
-		signo = SIGTERM;
-		break;
-	case O_SIGBREAK:
-		signo = SIGBREAK;
-		break;
-	case O_SIGABRT:
-		signo = SIGABRT;
-		break;
-	}
-	previousHandler = signal(signo, osig->handler);
-	return previousHandler;
->>>>>>> b42339f86429495f7f8c0eabe515682d6c9388ad
 }
